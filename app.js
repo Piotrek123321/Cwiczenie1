6b48e9d44b8a5a9f936427d9530dd5a56098af09
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-03-11 20:08:54 +0100

    zadanie5

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http=require("http");
 const student = require('./student');
-const server = http.createServer().listen(3000);
-console.log(`Server is running on ${server.address().port}`)
-console.log(`My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentid()}`);
+const server = http.createServer((req,res)=>{
+res.writeHead(200,{'Content-Type': 'text/html'});
+res.write(`My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentid()}`)
+}).listen(3000);
+console.log(`Server is running on ${server.address().port}`);
