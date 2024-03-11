1bd07be6363611aec512db7da8cce8057d96d6b3
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-03-11 19:58:08 +0100

    zadanie4

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
+const http=require("http");
 const student = require('./student');
+const server = http.createServer().listen(3000);
+console.log(`Server is running on ${server.address().port}`)
 console.log(`My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentid()}`);
