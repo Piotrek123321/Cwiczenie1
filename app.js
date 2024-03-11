fecfe634601e256a84297836d897eb66342b7a3f
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-03-11 19:50:19 +0100

    zadanie3

---
+++ app.js
@@ -0,0 +1,2 @@
+const student = require('./student');
+console.log(`My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentid()}`);
--- student.js
+++ student.js
@@ -1,8 +1,13 @@
-console.log('My name is Piotr Żuławnik. My student ID is 48689');
+
 
 function getStudentFullName(){
 return "Piotr Żuławnik";
 }
 function getStudentid(){
 return "48689";
+}
+
+module.exports={
+    getStudentFullName,
+    getStudentid,
 }
