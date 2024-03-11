f619cbf4ce730d43997d7c930ce543d1f64cac2a
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-03-11 20:15:15 +0100

    zadanie6

--- student.js
+++ student.js
@@ -1,10 +1,28 @@
 
 
-function getStudentFullName(){
-return "Piotr Żuławnik";
+function getStudentFullName(id){
+    switch (id) {
+        case 0:
+            return "Piotr Żuławnik";
+        case 1:
+            return "Kacper Nowak";
+        case 2:
+            return "Maja Kowalska";
+        default:
+            return "Michał Wiśniewski";
+    }
 }
-function getStudentid(){
-return "48689";
+function getStudentid(id){
+    switch (id) {
+        case 0:
+            return "48689";
+        case 1:
+            return "111222";
+        case 2:
+            return "222333";
+        default:
+            return "333444";
+    }
 }
 
 module.exports={
